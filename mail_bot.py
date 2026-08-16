f02240c234102544085982fb1b4bb5fd5cb7a3d2
sgdev <s07114587@gmail.com>
2026-08-16 18:55:52 +0530

    Update main website URL in mail response

--- mail_bot.py
+++ mail_bot.py
@@ -63,7 +63,7 @@ def send_brevo_api_reply(to_email, original_subject):
             f"Thanks for mailing us. Have a nice day!\n\n"
             f"🇮🇳 Proudly from India 🇮🇳\n\n"
             f"--- Our Profiles & Main Web ---\n"
-            f"🌐 Main Website: https://shubhomoy.dnc.su/\n"
+            f"🌐 Main Website: https://shubhomoy.vercel.app/\n"
             f"📧 Contact Email: sgdev@netc.fr\n"
             f"🎨 DeviantArt: https://www.deviantart.com/sgdev111\n"
             f"💼 Behance: https://www.behance.net/sgdev1\n"
